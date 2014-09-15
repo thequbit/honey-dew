7421d9a99736b679e438d387c40ea62eed911052
thequbit <thequbit@gmail.com>
2014-09-14 21:30:06 -0400

    got login working

--- honey-dew/honeydew/views.py
+++ honey-dew/honeydew/views.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import urllib
 
 from pyramid.response import Response
 from pyramid.view import view_config
@@ -22,6 +23,10 @@ def login(request):
 
          username = request.GET['username']
          password = request.GET['password']
+
+         # remove url encoding:
+         username = urllib.unquote(username).decode('utf8')
+         password = urllib.unquote(password).decode('utf8')
 
          success,user = Users.login_user(
              DBSession,
@@ -178,29 +183,4 @@ def get_users(request):
 
     resp = json.dumps(response)
     return Response(resp,content_type="application/json")
-
-#@view_config(route_name='home', renderer='templates/mytemplate.pt')
-#def my_view(request):
-#    try:
-#        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-#    except DBAPIError:
-#        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-#    return {'one': one, 'project': 'honey-dew'}
-#
-#
-#conn_err_msg = """\
-#Pyramid is having a problem using your SQL database.  The problem
-#might be caused by one of the following things:
-#
-#1.  You may need to run the "initialize_honey-dew_db" script
-#    to initialize your database tables.  Check your virtual
-#    environment's "bin" directory for this script and try to run it.
-#
-#2.  Your database server may not be running.  Check that the
-#    database server referred to by the "sqlalchemy.url" setting in
-#    your "development.ini" file is running.
-#
-#After you fix the problem, please restart the Pyramid application to
-#try it again.
-#"""
 
